6ddf85a820dcd8e0768aa7cf374c1d52941f125a
Zerdov <juleswillig08@gmail.com>
2025-05-13 11:05:33 +0200

    Start of server.ts

---
+++ src/controllers/home.ts
@@ -0,0 +1,13 @@
+import http from 'http';
+
+export const index = (req: http.IncomingMessage, res: http.ServerResponse<http.IncomingMessage> & {
+  req: http.IncomingMessage;
+}) => { 
+  if (req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'text' });
+    res.end('GET METHOD');
+  } else {
+    res.writeHead(200, { 'Content-Type': 'text' });
+    res.end('POST METHOD');
+  }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,22 @@
-console.log("Hello, Node.js + TypeScript!");
+// Agit comme le MainController: écoute et redirige vers le bon controller
+import http from 'http';
+import { index as home } from './controllers/home';
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text' });
+    res.end('Log in');
+    return;
+  };
+  if (req.url === '/home') {
+    home(req, res);
+    return;
+  };
+  res.writeHead(404, { 'Content-Type': 'text' });
+  res.end('Not found');
+  return;
+});
+
+server.listen(3000, () => {
+  console.log('Serveur démarré sur http://localhost:3000');
+});
